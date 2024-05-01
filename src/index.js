722fb9c850c89a907c58b9ae2de6fa0be493adf8
ahmadharkouss <71528971+ahmadharkouss@users.noreply.github.com>
2024-05-01 20:36:48 +0200

    test1: new workflow

--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
+//api url https://3cml39nmjxzysqjovm9gaqewng60ewbkj0677s8jovm.devgains.com/api/
+const express = require('express');
 const {connectDB} = require('./connect-db');
 
-connectDB();
+// Create Express app
+const app = express();
+const port = 3000;
 
+// Define a sample API endpoint
+app.get('/api/sample', (req, res) => {
+    res.json({ message: 'This is a sample API endpoint test test8' });
+});
+//Api to test connection to database
+app.get('/api/connectDb', async (req, res) => {
+    const connection = await connectDB();
+    res.json({ message: connection });
+});
 
-
+// Start the server
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
